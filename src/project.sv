4157cb05160e256e00ef8c9da3c4175bef418a5d
leonllrmc <leonllrcraft@gmail.com>
2026-08-28 15:16:29 +0200

    reducing ram to 12 bytes

--- src/project.sv
+++ src/project.sv
@@ -154,7 +154,7 @@ wire rst_n = rst_n_ext;
 
 
 // 64 bytes of ram for now
-  reg [7:0] RAM_data [15:0];
+  reg [7:0] RAM_data [11:0];
   integer ram_idx;
 
 
@@ -250,7 +250,7 @@ wire rst_n = rst_n_ext;
           currentState <= STATE_SPI_RD;
 
             if(hepiarisc_instruction_memop_wr) begin
-              if(hepiarisc_memop_address < 16) begin
+              if(hepiarisc_memop_address < 12) begin
                 RAM_data[hepiarisc_memop_address[2:0]] <= hepiarisc_memop_output;
               end else begin
               if(hepiarisc_memop_address == 8'h17) begin
@@ -261,7 +261,7 @@ wire rst_n = rst_n_ext;
             
 
             if(hepiarisc_instruction_memop_rd) begin
-              if(hepiarisc_memop_address < 16) begin
+              if(hepiarisc_memop_address < 12) begin
                 hepiarisc_memop_input <= RAM_data[hepiarisc_memop_address[3:0]];
               end
             end
