149274fccb898477673354d4461cb69845c15e11
leonllrmc <leonllrcraft@gmail.com>
2026-09-06 11:32:24 +0200

    I said let there be more tiles and there was more tiles

--- src/project.sv
+++ src/project.sv
@@ -154,7 +154,7 @@ wire rst_n = rst_n_ext;
 
 
 // 64 bytes of ram for now
-  reg [7:0] RAM_data [11:0];
+  reg [7:0] RAM_data [63:0];
   integer ram_idx;
 
 
@@ -169,9 +169,9 @@ wire rst_n = rst_n_ext;
 
       extflash_spi_cs <= 1'b1;
 
-      //for (ram_idx = 0; ram_idx < 64; ram_idx = ram_idx + 1) begin
-      //  RAM_data[ram_idx] <= 8'h00;
-      //end
+      for (ram_idx = 0; ram_idx < 64; ram_idx = ram_idx + 1) begin
+        RAM_data[ram_idx] <= 8'h00;
+      end
     end else begin
     case (currentState)
         STATE_SPI_RD: begin
@@ -250,8 +250,8 @@ wire rst_n = rst_n_ext;
           currentState <= STATE_SPI_RD;
 
             if(hepiarisc_instruction_memop_wr) begin
-              if(hepiarisc_memop_address < 12) begin
-                RAM_data[hepiarisc_memop_address[2:0]] <= hepiarisc_memop_output;
+              if(hepiarisc_memop_address < 64) begin
+                RAM_data[hepiarisc_memop_address[4:0]] <= hepiarisc_memop_output;
               end else begin
               if(hepiarisc_memop_address == 8'h17) begin
                 reg_rgb[2:0] <= hepiarisc_memop_output[3:0];
@@ -261,8 +261,8 @@ wire rst_n = rst_n_ext;
             
 
             if(hepiarisc_instruction_memop_rd) begin
-              if(hepiarisc_memop_address < 12) begin
-                hepiarisc_memop_input <= RAM_data[hepiarisc_memop_address[3:0]];
+              if(hepiarisc_memop_address < 64) begin
+                hepiarisc_memop_input <= RAM_data[hepiarisc_memop_address[4:0]];
               end
             end
         end
